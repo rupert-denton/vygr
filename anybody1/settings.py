a4d71e3087ba2ba0ba87461507858d49eab3a161
deadant88 <67182833+deadant88@users.noreply.github.com>
2022-01-20 06:30:00 +1100

    edited settings file re debug

--- anybody1/settings.py
+++ anybody1/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ["*"]
 
