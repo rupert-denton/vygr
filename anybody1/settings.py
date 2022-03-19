605d239f56a6146a806bfa8e62842f8e708c8da7
deadant88 <67182833+deadant88@users.noreply.github.com>
2022-03-19 21:58:51 +1300

    changed smtp password

--- anybody1/settings.py
+++ anybody1/settings.py
@@ -201,7 +201,7 @@ STATICFILES_DIRS = (
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'info@vygrapp.com'
-EMAIL_HOST_PASSWORD = '160788#rdE'
+EMAIL_HOST_PASSWORD = '160788#Rde'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
